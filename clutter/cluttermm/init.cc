541cd077744a66026b21f4de374dcbc3d6719bb0
Murray Cumming <murrayc@src.gnome.org>
2008-07-14 08:28:28 +0000

    do i need to commit this?
    svn path=/clutter-gtkmm/branches/cluttermm-0.6/; revision=1602

---
+++ clutter/cluttermm/init.cc
@@ -0,0 +1,38 @@
+// -*- c++ -*-
+/*
+ * Copyright 2008 Jonathon Jongsma
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free
+ * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <glibmm/init.h>
+#include <glibmm/ustring.h>
+#include <gdkmm/wrap_init.h>
+#include <cluttermmconfig.h> //For LIBCLUTTERMM_VERSION
+#include <cluttermm/wrap_init.h>
+#include <clutter/clutter.h>
+
+namespace Clutter
+{
+
+void init(int* nargs, gchar **args[])
+{
+  Glib::init(); //Sets up the g type system and the Glib::wrap() table.
+  Gdk::wrap_init();
+  wrap_init(); //Tells the Glib::wrap() table about the libcluttermm classes.
+  clutter_init(nargs, args);
+}
+
+} //namespace Clutter
---
+++ tests/test-alpha-creation.cc
@@ -0,0 +1,41 @@
+#include <cluttermm.h>
+#include <clutter/clutter.h>
+#include <iostream>
+
+Glib::RefPtr<Clutter::Actor> hand;
+
+int
+main (int argc, char *argv[])
+{
+    // initialize the C++ wrapper types
+    Clutter::init(&argc, &argv);
+
+    Glib::RefPtr<Clutter::Stage> stage = Clutter::Stage::get_default ();
+
+    // Create a timeline to manage animation
+    Glib::RefPtr<Clutter::Timeline> timeline =
+        Clutter::Timeline::create (360, 60); // num frames, fps
+
+    // Set up some behaviours to handle scaling
+    //Causes a Gobject (refcount?) warning: Glib::RefPtr<Clutter::Alpha> alpha =
+    //    Clutter::Alpha::create (timeline, Clutter::ALPHA_SINE);
+    Glib::RefPtr<Clutter::Alpha> alpha =
+        Clutter::Alpha::create (timeline, CLUTTER_ALPHA_SINE);
+
+    Glib::RefPtr<Clutter::BehaviourScale> scaler_1 =
+        Clutter::BehaviourScale::create (alpha,
+                0.5, 0.5,
+                1.0, 1.0,
+                Clutter::GRAVITY_CENTER);
+
+    // Show everying ( and map window )
+    stage->show ();
+
+    // and start it
+    timeline->start ();
+
+    clutter_main ();
+
+    return 0;
+}
+
